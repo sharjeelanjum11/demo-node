e6bffeafcd3efe9dc3b66df9cdf93c9a7119b58a
Sharjeel Anjum <sharjeelanjum12@gmail.com>
2023-03-08 10:42:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
+// Importing necessary modules
+const express = require('express');
 
-
+// Creating an instance of express app
 const app = express();
 
+// Setting up a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, I am Sharjeel');
+});
 
-
-app.get("/",(req,res)=>{
-    res.send("hello i am sharjeel");
-
-})
-
-app.listen(3001);
+// Starting the server
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
+});
